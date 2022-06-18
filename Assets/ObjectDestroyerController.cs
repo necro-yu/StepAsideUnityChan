14ae10dd39ed02457f2cf39bb829203e2a2e60e4
ねくろまんさ <75344908+necro-yu@users.noreply.github.com>
2022-06-18 17:32:10 +0900

    task_take2
    物体にリジットボディを追加して、ユニティちゃんの判定を参考に、コーン・車・コインを削除する。ユニティちゃんに合わせて止める。　の処理を追加。

--- Assets/ObjectDestroyerController.cs
+++ Assets/ObjectDestroyerController.cs
@@ -4,34 +4,37 @@ using UnityEngine;
 
 public class ObjectDestroyerController : MonoBehaviour
 {
-    //Unityちゃんのオブジェクト
-    private GameObject unitychan;
-    //Unityちゃんとオブジェクトデストロイヤーの距離
-    private float difference;
+    //Unityちゃんの前方向の速度
+    /// <summary>
+    /// 速度[m/s]
+    /// </summary>
+    private Vector3 _velocity = new Vector3(0, 0, 16f);
 
     // Use this for initialization
     void Start()
     {
-        //Unityちゃんのオブジェクトを取得
-        this.unitychan = GameObject.Find("unitychan");
-        //Unityちゃんとの位置（z座標）の差を求める
-        this.difference = unitychan.transform.position.z - this.transform.position.z;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Unityちゃんの位置に合わせてオブジェクトデストロイヤーの位置を移動
-        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+        transform.position = transform.position + (_velocity * Time.deltaTime);
     }
 
     /// <summary>
-    /// 衝突判定時の動作
+    /// 衝突判定
     /// </summary>
-    /// <param name="collision"></param>
-    private void OnCollisionEnter(Collision collision)
+    /// <param name="other">相手の物体</param>
+    private void OnTriggerEnter(Collider other)
     {
-        // ぶつかった相手を破壊する。
-        GameObject.Destroy(collision.gameObject);
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag")
+        {
+            // ぶつかった相手を破壊する。
+            GameObject.Destroy(other.gameObject);
+        }
+
+        // ぶつかった相手のタグを確認
+        Debug.Log(other.gameObject.tag);
     }
 }
